348c36a11dfbc0274dd6e661b1a0e5a13c18d9a0
Alfredo Sánchez <alfred3ks@gmail.com>
2021-12-28 14:46:23 +0100

    Funcion constructora de objetos, palabra new.

---
+++ 15-objetos-funcion-constructora.js
@@ -0,0 +1,41 @@
+/*
+
+Objetos, funcion constructora: Generaremos un template del objeto y luego vamos a construir los objetos.
+
+Construimos nuestro template: Recibe las propiedades por parametro:
+Tambien por convension llamaremos en mayuscula la funcion:
+
+    function Auto(marca, modelo, annio){
+        this.marca = marca;
+        this.modelo = modelo;
+        this.annio = annio;
+
+    }
+
+Ahora para construir un objeto necesitariamos la palabra reservada new:
+
+    let miAuto = ew Auto("Toyota", "Corolla", 2000);
+    miAuto.marca;
+    miAuto.modelo;
+    miAuto.annio;
+
+*/
+
+function Auto(marca, modelo, annio){
+        this.marca = marca;
+        this.modelo = modelo;
+        this.annio = annio;
+    }
+
+
+let auto1= new Auto("Toyota", "Corolla",2000);
+console.log(auto1.marca);
+console.log(auto1.modelo);
+console.log(auto1.annio);
+
+let auto2 = new Auto("Tesla", "Model 3", 2010);
+console.log(auto2.marca);
+console.log(auto2.modelo);
+console.log(auto2.annio);
+
+
